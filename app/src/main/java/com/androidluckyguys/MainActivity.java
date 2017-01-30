a53064196a3d3f805b9b2c82af4f8861525a1dfa
Lucky <lucky.rana@xchanging.com>
2017-01-30 18:14:40 +0800

    min api change

--- app/src/main/java/com/androidluckyguys/MainActivity.java
+++ app/src/main/java/com/androidluckyguys/MainActivity.java
@@ -43,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        checkLocationPermission(activity,context,LOCATION_PERMISSION);
+
+        //checkLocationPermission(activity,context,LOCATION_PERMISSION);
 
 
     }
